ec5252c09f579d31076695dee7ff95d74c88c814
gabs1san <sanbiel398@gmail.com>
2025-06-18 19:26:34 -0300

    Testando os conceitos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace Linq
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, };
+
+            //Dfine the query expression
+            var result = numbers.
+                Where(x => x % 2 == 0).
+                Select(x => x * 10);
+
+            //Execution the query
+            foreach (int x in result)
             {
-                
+                Console.WriteLine(x);
             }
+
+        }
     }
 }
